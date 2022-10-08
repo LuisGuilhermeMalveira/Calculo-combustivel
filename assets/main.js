867b1f68f2f482e6cc51286d494b2e5d076acb01
Luís Guilherme Theóphilo Malveira <97563186+LuisGuilhermeMalveira@users.noreply.github.com>
2022-10-08 03:32:28 -0300

    Imprimindo resultado da conta do combustivel

--- assets/main.js
+++ assets/main.js
@@ -6,7 +6,23 @@ function capturar() {
 
 	let valorKMG = valorG / autoG;
 	let valorKMA = valorA / autoA;
-
 	console.log(valorKMG);
 	console.log(valorKMA);
+
+	let textoInfo = document.querySelector("#textoInfo");
+
+	let textoResultado =
+		"O valor do Km da gasolina é " +
+		valorKMG.toFixed(2) * 100 +
+		" centavos e o do alcool é " +
+		valorKMA.toFixed(2) * 100 +
+		" centavos. ";
+
+	textoInfo.textContent = textoResultado;
 }
+
+/*if (valorKMA > valorKMG) {
+	let abasteca = "Abasteça com Álcool!";
+} else {
+	let abasteca = "Abasteça com Gasolina!";
+}*/
