6ec2732e81920923d234e2bf38e837199d9d43b5
Luís Guilherme Theóphilo Malveira <97563186+LuisGuilhermeMalveira@users.noreply.github.com>
2022-10-07 21:28:06 -0300

    Iniciando Javscript

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,12 @@
-var capturando = "";
-
 function capturar() {
-	capturando = document.getElementsByClassName("valorAlcool").value;
+	let autoG = document.querySelector("#autoGas").value;
+	let valorG = document.querySelector("#valorGas").value;
+	let valorA = document.querySelector("#valorAlcool").value;
+	let autoA = document.querySelector("#autoAlcool").value;
 
-	console.log(capturando);
+	let valorKMG = valorG / autoG;
+	let valorKMA = valorA / autoA;
+
+	console.log(valorKMG);
+	console.log(valorKMA);
 }
