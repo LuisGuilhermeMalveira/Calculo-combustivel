313495486f0c1234f748319724e9af0dcff45f74
Luís Guilherme Theóphilo Malveira <97563186+LuisGuilhermeMalveira@users.noreply.github.com>
2022-11-03 22:33:29 -0300

    Inclusão de algumas validações.

--- assets/main.js
+++ assets/main.js
@@ -9,48 +9,68 @@ function capturar() {
 
 	let textoInfo = document.querySelector("#textoInfo");
 
-	if (autoG > 100 || autoG < 0) {
-		textoInfo.classList.add("erro");
-		textoInfo.textContent =
-			"Insira um valor entre 0 e 100 no campo: Autonomia na gasolina.";
-	} else if (valorG > 10 || valorG < 0) {
-		textoInfo.classList.add("erro");
-		textoInfo.textContent =
-			"Insira um valor entre 0 e 10 no campo: Valor da gasolina.";
-	} else if (autoA > 100 || valorG < 0) {
-		textoInfo.classList.add("erro");
-		textoInfo.textContent =
-			"Insira um valor entre 0 e 100 no campo: Autonomia no álcool.";
-	} else if (valorA > 10 || valorG < 0) {
-		textoInfo.classList.add("erro");
-		textoInfo.textContent =
-			"Insira um valor entre 0 e 10 no campo: Valor no álcool.";
+	let mensagemErro = [
+		`O valor do Km da gasolina é ${
+			valorKMG.toFixed(2) * 100
+		} centavo o Km do álcool é ${
+			valorKMA.toFixed(2) * 100
+		} centavos. Então é melhor abastecer com gasolina.`,
+
+		`O valor do Km da gasolina é ${
+			valorKMG.toFixed(2) * 100
+		} centavo o Km do álcool é ${
+			valorKMA.toFixed(2) * 100
+		} centavos. Então é melhor abastecer com álcool.`,
+
+		`O valor do Km da gasolina é ${
+			valorKMG.toFixed(2) * 100
+		} centavo o Km do álcool é ${
+			valorKMA.toFixed(2) * 100
+		} centavos. Então não faz diferença o combustível.`,
+	];
+
+	if (valorA && valorG && autoA && autoG) {
+		mensagemResultado();
+		validacao();
 	} else {
+		textoInfo.classList.add("erro");
+		textoInfo.textContent = "Favor inserir todos os dados!";
+	}
+
+	function validacao() {
+		if (autoG > 100 || autoG < 0) {
+			textoInfo.classList.add("erro");
+			textoInfo.textContent =
+				"Insira um valor entre 0 e 100 no campo: Autonomia na gasolina.";
+		} else if (valorG > 10 || valorG < 0) {
+			textoInfo.classList.add("erro");
+			textoInfo.textContent =
+				"Insira um valor entre 0 e 10 no campo: Valor da gasolina.";
+		} else if (autoA > 100 || valorG < 0) {
+			textoInfo.classList.add("erro");
+			textoInfo.textContent =
+				"Insira um valor entre 0 e 100 no campo: Autonomia no álcool.";
+		} else if (valorA > 10 || valorG < 0) {
+			textoInfo.classList.add("erro");
+			textoInfo.textContent =
+				"Insira um valor entre 0 e 10 no campo: Valor no álcool.";
+		}
+	}
+
+	function mensagemResultado() {
 		if (valorKMA > valorKMG) {
 			textoInfo.classList.remove("erro");
-			let textoResultado =
-				"O valor do Km da gasolina é " +
-				valorKMG.toFixed(2) * 100 +
-				" centavos e o Km do alcool é " +
-				valorKMA.toFixed(2) * 100 +
-				" centavos. Então é melhor abastecer com gasolina.";
+			let textoResultado = mensagemErro[0];
+
 			textoInfo.textContent = textoResultado;
 		} else if (valorKMA < valorKMG) {
 			textoInfo.classList.remove("erro");
-			let textoResultado =
-				"O valor do Km da gasolina é " +
-				valorKMG.toFixed(2) * 100 +
-				" centavos e o Km do alcool é " +
-				valorKMA.toFixed(2) * 100 +
-				" centavos. Então é melhor abastecer com álcool.";
+			let textoResultado = mensagemErro[1];
+
 			textoInfo.textContent = textoResultado;
 		} else {
 			textoInfo.classList.remove("erro");
-			let textoResultado = `O valor do Km da gasolina é ${
-				valorKMG.toFixed(2) * 100
-			} centavo o Km do álcool é ${
-				valorKMA.toFixed(2) * 100
-			} centavos. Então não faz diferença o combustível.`;
+			let textoResultado = mensagemErro[2];
 
 			textoInfo.textContent = textoResultado;
 		}
